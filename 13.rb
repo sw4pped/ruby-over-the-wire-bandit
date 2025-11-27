0ebf36dccb6eaf7bd04468e2b41fa29f8caa178b
sw4pped <77627612+sw4pped@users.noreply.github.com>
2025-11-27 13:44:00 -0300

    Added missing part for level 13, getting password for level 14

--- 13.rb
+++ 13.rb
@@ -3,9 +3,14 @@ require 'net/ssh'
 
 Net::SSH.start('bandit.labs.overthewire.org', 'bandit13', :password => 'FO5dwFsc0cbaIiH0h8J2eUks2vdTDwAn', :port => '2220') do |ssh|
   command = ssh.exec!("cat sshkey.private").chomp
-  puts '[+] Saving private key'
+  puts '[+] Saving private key...'
   File.open('sshkey.private', 'w') do |key|
     key.puts(command)
   end
-  puts '[+] Private key successful'
+  puts '[+] Private key saved successfully'
+end
+
+Net::SSH.start('bandit.labs.overthewire.org', 'bandit14', :keys => "sshkey.private", :port => '2220') do |ssh|
+  command = ssh.exec!("cat /etc/bandit_pass/bandit14").chomp
+  puts command
 end
